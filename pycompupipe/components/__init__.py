e49a585ba9b39a19d4943b26d232d3166ce3189a
xaedes <xaedes@gmail.com>
2016-01-03 18:30:14 +0100

    moved files

--- pycompupipe/components/__init__.py
+++ pycompupipe/components/__init__.py
@@ -2,29 +2,30 @@
 # -*- coding: utf-8 -*-
 from gui import *
 from processing import *
+from pygame_components import *
+
+from print_entity_structure import PrintEntityStructure
+
 
 from pose import Pose
 from size import Size
 from anchor import Anchor
+from pose_transform import PoseTransform
+from snap_to_grid import SnapToGrid
+
 from bounding_box import BoundingBox
 
-from pygame_component import Pygame
-from blocking_pygame_event_pump import BlockingPygameEventPump
-from draw_on_resized import DrawOnResized
 
 from selectable import Selectable
 from draggable import Draggable
+
+
 from draw_bounding_box import DrawBoundingBox
-from color_fill import ColorFill
-from pygame_surface import PygameSurface
-from surface_draw_event import SurfaceDrawEvent
-from blit_surface import BlitSurface
-from resize_event_on_videoresize import ResizeEventOnVideoresize
-from print_entity_structure import PrintEntityStructure
 from draw_process_connectors import DrawProcessConnectors
+
+
+
 from draw_grid import DrawGrid
-from snap_to_grid import SnapToGrid
-from pose_transform import PoseTransform
 from draw_line import DrawLine
 from occupancy_grid import OccupancyGrid
 from occupying_bounding_box import OccupyingBoundingBox
---
+++ pycompupipe/components/pygame_components/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from pygame_component import Pygame
+from pygame_surface import PygameSurface
+from blit_surface import BlitSurface
+from blocking_pygame_event_pump import BlockingPygameEventPump
+from color_fill import ColorFill
+from draw_on_resized import DrawOnResized
+from resize_event_on_videoresize import ResizeEventOnVideoresize
+from surface_draw_event import SurfaceDrawEvent
