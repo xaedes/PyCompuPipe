f418a2c24a3f8510dac1aae69bf6eb3bfcded3db
xaedes <xaedes@gmail.com>
2016-01-03 21:25:39 +0100

    testcases for BlitSurface

--- pycompupipe/components/pygame_components/blit_surface.py
+++ pycompupipe/components/pygame_components/blit_surface.py
@@ -17,21 +17,14 @@ class BlitSurface(Component):
         self.surface = None
 
 
-    @callback
-    @with_components(required=[PygameSurface])
-    def awake(self, pygamesurface):
-        self.surface = pygamesurface
-        
     @component_callback
-    def component_attached(self):
+    @with_components(required=[PygameSurface])
+    def component_attached(self,pygamesurface):
         self.entity.register_callback(self.draw_event_name, self.draw)
+        self.surface = pygamesurface
 
     def draw(self, surface):
-        xy = self.entity.fire_callbacks_pipeline("position")
-        if xy is None:
-            x,y = 0,0
-        else:
-            x,y = xy
+        x, y = self.entity.fire_callbacks_pipeline("position") or (0,0)
         # print "blit", self.surface.surface, "at", (x,y), "to", self.draw_event_name
         surface.blit(self.surface.surface,(x,y),None,self.blit_flags)
 
---
+++ tests/components/pygame_components/test_blit_surface.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+from pyecs import *
+from pycompupipe.components import PygameSurface, BlitSurface, GuiElement
+
+import mock
+import pygame
+import re
+import os.path
+
+from testing import *
+
+class TestBlitSurface():
+
+    @mock.patch("pycompupipe.components.pygame_components.blit_surface.BlitSurface.draw")
+    def test_event(self, mocked_draw):
+        e = Entity()
+        s = e.add_component(PygameSurface((10,10)))
+        d = e.add_component(BlitSurface("draw",10))
+        screen = mock.NonCallableMock()
+        e.fire_callbacks("draw", screen)
+        mocked_draw.assert_called_once_with(screen)
+
+    def test_draw(self):
+        size = (100,100)
+        size2 = (100,50)
+        
+        screen = pygame.Surface(size)
+
+        screen.fill((0,0,0))
+
+        e = Entity()
+        s = e.add_component(PygameSurface(size2))
+        d = e.add_component(BlitSurface("draw"))
+
+        s.surface.fill((255,255,255))
+        d.draw(screen)
+
+        assert (pygame.surfarray.pixels3d(screen)[:size2[0],:size2[1]] == (255,255,255)).all()
+        assert (pygame.surfarray.pixels3d(screen)[:size2[0],size2[1]:] == (0,0,0)).all()
+
+    
+    @forEach("x",lambda:iter([0,50,80]))
+    @forEach("y",lambda:iter([0,50,80]))
+    def test_draw_xy(self, x, y):
+        size = (100,100)
+        size2 = (1,1)
+        
+        screen = pygame.Surface(size)
+
+        screen.fill((0,0,0))
+
+        e = Entity()
+        s = e.add_component(PygameSurface(size2))
+        g = e.add_component(GuiElement((x,y)))
+        d = e.add_component(BlitSurface("draw"))
+
+        s.surface.fill((255,255,255))
+        d.draw(screen)
+
+        assert (pygame.surfarray.pixels3d(screen)[x,y] == (255,255,255)).all()
+
+    def test_str(self):
+        d = BlitSurface("draw")
+        assert str(d) == "BlitSurface(draw)"
+        
