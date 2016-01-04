e8f023f79cff1451a76a85e7ef94401d4389c9d2
xaedes <xaedes@gmail.com>
2016-01-04 01:40:05 +0100

    test ResizeEventOnVideoresize

---
+++ tests/components/pygame_components/test_resize_event_on_videoresize.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+from pyecs import *
+from pycompupipe.components import ResizeEventOnVideoresize
+
+import mock
+import pygame
+
+from testing import *
+
+class TestResizeEventOnVideoresize():
+
+    def test(self):
+        e = Entity()
+        d = e.add_component(ResizeEventOnVideoresize())
+
+        mocked_resize = mock.MagicMock()
+        mocked_event = mock.NonCallableMock()
+        e.register_callback("resize", mocked_resize)
+
+        e.fire_callbacks("videoresize", mocked_event)
+
+        mocked_resize.assert_called_once_with(mocked_event.size)
