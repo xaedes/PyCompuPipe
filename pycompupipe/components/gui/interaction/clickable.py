a4e92d22ba79842235e1b9d93647eb35b8657cea
xaedes <xaedes@gmail.com>
2016-01-06 22:16:16 +0100

    code cleanup

--- pycompupipe/components/gui/interaction/clickable.py
+++ pycompupipe/components/gui/interaction/clickable.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-from __future__ import division    # Standardmäßig float division - Ganzzahldivision kann man explizit mit '//' durchführen
+from __future__ import division
 
 from pyecs import *
-from pycompupipe.other import Event
 # from pyecs.components import *
-from .. import GuiElement
-
-from time import time
 
 class Clickable(Component):
     """docstring for Clickable"""
